e5891b7f2dc2d1ea72d160080be9fce59ae59169
ImamHossain-eng <babubpx2762@gmail.com>
2022-09-19 22:02:11 +0600

    Design the navbar

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+
+<nav class="navbar navbar-expand-lg bg-dark py-3 navbar-dark">
+        <div class="container">
+            <a href="#" class="navbar-brand">Imam Hossain</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navmenu">
+                <ul class="navbar-nav ms-auto">
+                    <li class="navbar-list">
+                      <RouterLink to="/" class="nav-link">Home</RouterLink>  
+                    </li>
+                    <li class="navbar-list">
+                      <RouterLink to="/project" class="nav-link">Projects</RouterLink>  
+                    </li>
+                    <li class="navbar-list">
+                      <RouterLink to="/education" class="nav-link">Educational Qualifications</RouterLink>  
+                    </li>
+                    <li class="navbar-list">
+                      <RouterLink to="/skill" class="nav-link">Skills</RouterLink>  
+                    </li>
+                    <li class="navbar-list">
+                      <RouterLink to="#" class="nav-link">Experience</RouterLink>  
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      info: '',
+      api: 'https://resume.likemebd.com/api/v1/match/1'
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+    axios.get(this.api)
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err))
+  }
+}
+</script>
+
+<template>
+  <main>
+   
+  </main>
+</template>
